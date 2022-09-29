f792e60a4e59be2d96d4abfb6c38d2ef97d16820
RAJENDRA YADAV <rjyadav0230@gmail.com>
2022-09-29 19:26:36 +0530

    Top And Nav Added

--- frontend/src/HomePage/Homepage.jsx
+++ frontend/src/HomePage/Homepage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LpNavbar from "./Components/LpNavbar";
+import LpTop from "./Components/LpTop";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <LpTop />
+      <LpNavbar />
+    </>
+  );
 };
 
 export default Homepage;
